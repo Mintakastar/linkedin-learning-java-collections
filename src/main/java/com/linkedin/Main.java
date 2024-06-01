354d168effc1e1ec8261d43f15f632b8d72975f8
Raffenio Lopez <mintaka.star@gmail.com>
2024-05-31 18:50:30 -0700

    me falto

--- src/main/java/com/linkedin/Main.java
+++ src/main/java/com/linkedin/Main.java
@@ -1,7 +1,24 @@
 package com.linkedin;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Map<Integer,Integer> dups = new HashMap<>();
+        dups.put(5,1);
+        dups.put(5,2);
+
+        dups.put(6,1);
+        dups.put(6,2);
+
+        dups.put(7,1);
+
+        Set<Map.Entry<Integer, Integer>> entries = dups.entrySet();
+
     }
 }
+
